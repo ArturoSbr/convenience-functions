5829ab92eeb912f890afc2e70781ee859fa7a7f8
Arturo Soberon <soberonarturo@gmail.com>
2024-01-10 17:05:29 -0600

    Completed docstring for get_last

--- dates/dates.py
+++ dates/dates.py
@@ -2,11 +2,24 @@
 from datetime import datetime, timedelta
 
 
-# Function to rewind current date to last day `m` months ago
-def get_last_day(months_ago=1, format='%Y-%m-%d'):
-    """
-    This function rewinds the date back to the last day of the month from
+# Function to rewind current date to last day `n` months ago
+def get_last_day(months_ago=1, format='%Y-%m-%d') -> str:
+    """This function rewinds the date back to the last day of the month from
     `months_ago` months ago.
+
+    Parameters
+    ----------
+    months_ago : int, default 1
+        The number of months to rewind the date back to.
+    format: str, default '%Y-%m-%d'
+        Format string for the date returned by the function.
+
+    Returns
+    -------
+    str
+        Returns the last day of the month from `months_ago` months ago as a
+        string (formatted according to `format`).
+
     """
 
     # Rewind current date `months_ago` times
@@ -18,11 +31,11 @@ def get_last_day(months_ago=1, format='%Y-%m-%d'):
     return ret.strftime(format)
 
 
-# Function to rewind current date back to first day from `n` months ago
+# Function to rewind current date back to first day from `n` months ago
 def get_first_day(months_ago=1, format='%Y-%m-%d'):
-    """
-    This function rewinds the date back to the first day of the month from
+    """This function rewinds the date back to the first day of the month from
     `months_ago` months ago.
+
     """
 
     # Use get_last_day and replace day to 1
